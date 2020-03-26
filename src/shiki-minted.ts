845192e66179ecbbd8a8d063d9e979dfb026cd9a
Leandro Facchinetti <git@leafac.com>
2020-03-26 18:01:14 -0400

    Add more debugging & preserve lines

--- src/shiki-minted.ts
+++ src/shiki-minted.ts
@@ -1,22 +1,27 @@
 #!/usr/bin/env node
 
-import yargs from "yargs";
-import fs from "fs";
-import { getHighlighter, loadTheme } from "shiki";
-import { TLang } from "shiki-languages";
-import { TTheme, IShikiTheme } from "shiki-themes";
-import { renderToLaTeX } from ".";
+import * as shiki from "shiki";
+import * as shikiHighlighter from "shiki/dist/highlighter";
+import * as shikiLanguages from "shiki-languages";
+import * as shikiThemes from "shiki-themes";
+import * as shikiLatex from ".";
+import * as yargs from "yargs";
+import * as fs from "fs";
 process.env.DEBUG_COLORS = "false";
 import createDebug from "debug";
 
 (async () => {
-  createDebug.log = (...args: any[]) =>
-    fs.appendFileSync("shiki-minted-debug.log", args + "\n");
+  createDebug.log = (message: string) =>
+    fs.appendFileSync("shiki-minted-debug.log", message + "\n");
   const debug = createDebug("shiki-latex");
   const themePath = "shiki-minted-theme.pyg";
-  const argv = yargs.array("P").argv;
-  debug(process.argv);
-  debug(argv);
+  const argv = yargs
+    .string("S")
+    .string("l")
+    .array("P")
+    .string("o").argv;
+  debug(`process.argv: ${JSON.stringify(process.argv, null, 2)}`);
+  debug(`argv: ${JSON.stringify(argv, null, 2)}`);
   const {
     S: themeToStore,
     l: language,
@@ -24,35 +29,64 @@ import createDebug from "debug";
     o: outputPath,
     _: [inputPath]
   } = argv;
-  if (themeToStore !== undefined)
-    return fs.writeFileSync(themePath, themeToStore);
+  if (themeToStore !== undefined) {
+    fs.writeFileSync(themePath, themeToStore);
+    debug(`Stored theme ‘${themeToStore}’ at ‘${themePath}’`);
+    process.exit();
+  }
   if (
     language !== undefined &&
     outputPath !== undefined &&
     inputPath !== undefined
   ) {
     let theme = "default";
-    if (fs.existsSync(themePath)) theme = fs.readFileSync(themePath, "utf8");
+    try {
+      theme = fs.readFileSync(themePath, "utf8");
+      debug(`Theme ‘${theme}’ selected from ‘${themePath}’`);
+    } catch {}
     if (options !== undefined) {
       for (const option of options) {
-        const [key, value] = (option as string).split("=");
-        if (key === "style") theme = value;
+        const [key, value] = option.toString().split("=");
+        if (key === "style") {
+          theme = value;
+          debug(`Theme ‘${theme}’ selected from command-line option ‘${key}’`);
+        }
       }
     }
     if (theme === "default") theme = "light_plus";
-    const highlighter = await getHighlighter({
-      theme: theme as TTheme | IShikiTheme
-    })
-      .catch(() => getHighlighter({ theme: loadTheme(theme) }))
-      .catch(() => {
-        console.error(`Failed to load theme: ${theme}`);
-        process.exit(1);
+    debug(`Final theme selection: ‘${theme}’`);
+    let highlighter: shikiHighlighter.Highlighter;
+    try {
+      highlighter = await shiki.getHighlighter({
+        theme: theme as shikiThemes.TTheme | shikiThemes.IShikiTheme
       });
-    const input = fs.readFileSync(inputPath, "utf8").trimRight();
-    const lines = highlighter.codeToThemedTokens(input, language as TLang);
-    debug(lines);
-    return fs.writeFileSync(outputPath as string, renderToLaTeX(lines));
+      debug(`Loaded built-in theme: ‘${theme}’`);
+    } catch {
+      try {
+        highlighter = await shiki.getHighlighter({
+          theme: shiki.loadTheme(theme)
+        });
+        debug(`Loaded theme file: ‘${theme}’`);
+      } catch {
+        console.error(`Failed to load theme: ‘${theme}’`);
+        process.exit(1);
+      }
+    }
+    const input = fs.readFileSync(inputPath, "utf8");
+    debug(`input: ‘${input}’`);
+    const lines = highlighter.codeToThemedTokens(
+      input.slice(0, input.length - 1),
+      language as shikiLanguages.TLang
+    );
+    debug(`lines: ${JSON.stringify(lines, null, 2)}`);
+    const latex = shikiLatex.renderToLaTeX(lines);
+    debug(`latex: ‘${latex}’`);
+    fs.writeFileSync(outputPath, latex);
+    process.exit();
   }
-  console.error(`Unrecognized invocation\n%j\n%j`, process.argv, argv);
+  console.error(`Unrecognized invocation
+${JSON.stringify(process.argv, null, 2)}
+${JSON.stringify(argv, null, 2)}
+`);
   process.exit(1);
 })();
