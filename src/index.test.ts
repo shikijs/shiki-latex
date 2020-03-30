87b529f3a8cd13edea444682661d5cd0172f034f
Leandro Facchinetti <git@leafac.com>
2020-03-30 13:20:12 -0400

    Add a test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,38 @@
 import { renderToLaTeX } from ".";
 import { getHighlighter } from "shiki";
 
-test("renderToLaTeX()", async () => {
-  const highlighter = await getHighlighter({ theme: "light_plus" });
-  const lines = highlighter.codeToThemedTokens(
-    `const name = "Leandro Facchinettti \\{}%$#";`,
-    "ts"
-  );
-  expect(renderToLaTeX(lines)).toMatchInlineSnapshot(`
-    "\\\\begin{Verbatim}[commandchars=\\\\\\\\\\\\{\\\\}]
-    \\\\textcolor[HTML]{0000FF}{const}\\\\textcolor[HTML]{000000}{ }\\\\textcolor[HTML]{001080}{name}\\\\textcolor[HTML]{000000}{ = }\\\\textcolor[HTML]{A31515}{\\"Leandro Facchinettti \\\\textbackslash{}\\\\{\\\\}%$#\\"}\\\\textcolor[HTML]{000000}{;}
-    \\\\end{Verbatim}
-    "
-  `);
+describe("renderToLaTeX()", () => {
+  test("basic rendering", async () => {
+    expect(
+      renderToLaTeX(
+        (
+          await getHighlighter({
+            theme: "light_plus"
+          })
+        ).codeToThemedTokens(`const name = "Leandro Facchinettti";`, "ts")
+      )
+    ).toMatchInlineSnapshot(`
+      "\\\\begin{Verbatim}[commandchars=\\\\\\\\\\\\{\\\\}]
+      \\\\textcolor[HTML]{0000FF}{const}\\\\textcolor[HTML]{000000}{ }\\\\textcolor[HTML]{001080}{name}\\\\textcolor[HTML]{000000}{ = }\\\\textcolor[HTML]{A31515}{\\"Leandro Facchinettti\\"}\\\\textcolor[HTML]{000000}{;}
+      \\\\end{Verbatim}
+      "
+    `);
+  });
+
+  test("strange characters", async () => {
+    expect(
+      renderToLaTeX(
+        (
+          await getHighlighter({
+            theme: "light_plus"
+          })
+        ).codeToThemedTokens(`const strangeCharacters = "\\{}%$#";`, "ts")
+      )
+    ).toMatchInlineSnapshot(`
+      "\\\\begin{Verbatim}[commandchars=\\\\\\\\\\\\{\\\\}]
+      \\\\textcolor[HTML]{0000FF}{const}\\\\textcolor[HTML]{000000}{ }\\\\textcolor[HTML]{001080}{strangeCharacters}\\\\textcolor[HTML]{000000}{ = }\\\\textcolor[HTML]{A31515}{\\"\\\\textbackslash{}\\\\{\\\\}%$#\\"}\\\\textcolor[HTML]{000000}{;}
+      \\\\end{Verbatim}
+      "
+    `);
+  });
 });
